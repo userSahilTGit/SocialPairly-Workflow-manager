44f6c3c5e9f1e4a8577cd843f28797eacc6e1404
userSahilTGit <sahil.t@socialpairly.com>
2026-07-01 17:34:16 +0000

    Project structure

---
+++ src/main/java/com/SocialPairly_Workflow_Manger/SocialPairlyWorkflowMangerApplication.java
@@ -0,0 +1,13 @@
+package com.SocialPairly_Workflow_Manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPairlyWorkflowMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPairlyWorkflowMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SocialPairly_Workflow_Manger/SocialPairlyWorkflowMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SocialPairly_Workflow_Manger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPairlyWorkflowMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
